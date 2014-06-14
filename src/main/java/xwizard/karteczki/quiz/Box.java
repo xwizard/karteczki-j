f4b6df8d293101455974a87958ce347a470c85ac
xwizard <xwizard@server>
2014-06-14 18:55:02 +0200

    Adding ang checing card implemented.

--- src/main/java/xwizard/karteczki/quiz/Box.java
+++ src/main/java/xwizard/karteczki/quiz/Box.java
@@ -1,5 +1,33 @@
 package xwizard.karteczki.quiz;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 public class Box {
+  
+  private final static int COMPARTMENT_AMOUNT = 5;
+  
+  List<List<UUID>> compartments;
+  
+  Box() {
+    compartments = new LinkedList<List<UUID>>();
+    for (int i = 0; i < COMPARTMENT_AMOUNT; i++) {
+      compartments.add(new LinkedList<UUID>());
+    }
+  }
+
+  public void addCard(int compartmentNumber, UUID cardId) {
+    compartments.get(compartmentNumber).add(cardId);
+  }
+
+  public boolean containsCard(UUID cardId) {
+    for (List<UUID> compartment : compartments) {
+      if (compartment.contains(cardId)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
--- src/test/java/xwizard/karteczki/quiz/BoxTest.java
+++ src/test/java/xwizard/karteczki/quiz/BoxTest.java
@@ -1,5 +1,19 @@
 package xwizard.karteczki.quiz;
 
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BoxTest {
 
+  @Test
+  public void addCard() {
+    Box box = new Box();
+    
+    UUID cardId = UUID.randomUUID();
+    box.addCard(1, cardId);
+    
+    Assert.assertTrue(box.containsCard(cardId));
+  }
 }
