02b74dd87b1eb215eb1365efcab800da69162c60
xwizard <xwizard@server>
2014-06-15 20:17:32 +0200

    Box.addCard (almost) complete.

--- src/main/java/xwizard/karteczki/quiz/Box.java
+++ src/main/java/xwizard/karteczki/quiz/Box.java
@@ -8,17 +8,27 @@ public class Box {
   
   private final static int COMPARTMENT_AMOUNT = 5;
   
+  UUID id;
   List<List<UUID>> compartments;
   
   Box() {
     compartments = new LinkedList<List<UUID>>();
+    id = UUID.randomUUID();
     for (int i = 0; i < COMPARTMENT_AMOUNT; i++) {
       compartments.add(new LinkedList<UUID>());
     }
   }
 
   public void addCard(int compartmentNumber, UUID cardId) {
+    if (cardId == null) throw new IllegalArgumentException("cardId cannot be null!");
+    if (isIllegal(compartmentNumber)) throw new IllegalArgumentException("Illegal compartment number: " + compartmentNumber);
+    if (containsCard(cardId)) throw new IllegalStateException("Card with id " + cardId + " already exists in box " + id);
+    
     compartments.get(compartmentNumber).add(cardId);
+  }
+
+  private boolean isIllegal(int compartmentNumber) {
+    return compartmentNumber < 0 || compartmentNumber >= COMPARTMENT_AMOUNT;
   }
 
   public boolean containsCard(UUID cardId) {
--- src/test/java/xwizard/karteczki/quiz/BoxTest.java
+++ src/test/java/xwizard/karteczki/quiz/BoxTest.java
@@ -3,17 +3,51 @@ package xwizard.karteczki.quiz;
 import java.util.UUID;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BoxTest {
 
+  private Box box;
+  private UUID cardId;
+  
+  @Before
+  public void setUp() {
+    box = new Box();
+    cardId = UUID.randomUUID();
+  }
+
   @Test
   public void addCard() {
-    Box box = new Box();
-    
-    UUID cardId = UUID.randomUUID();
     box.addCard(1, cardId);
     
     Assert.assertTrue(box.containsCard(cardId));
+  }
+  
+  @Test
+  public void addTwoCards() {
+    UUID cardId2 = UUID.randomUUID();
+    
+    box.addCard(1, cardId);
+    box.addCard(2, cardId2);
+    
+    Assert.assertTrue(box.containsCard(cardId));
+    Assert.assertTrue(box.containsCard(cardId2));
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void addCardWithNullId() {
+    box.addCard(0, null);
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void addCardToNotExistingCompartment(){
+    box.addCard(Integer.MAX_VALUE, cardId);
+  }
+  
+  @Test(expected = IllegalStateException.class)
+  public void addExistingCard() {
+    box.addCard(2, cardId);
+    box.addCard(0, cardId);
   }
 }
