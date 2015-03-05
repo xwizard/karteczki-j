a7d03d9e933a82d289f8fc7d9c036f9766b13c96
xwizard <xwizard@server>
2015-03-05 21:34:45 +0100

    Random thoughts.

--- src/main/java/xwizard/karteczki/events/DomainEvents.java
+++ src/main/java/xwizard/karteczki/events/DomainEvents.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xwizard.karteczki.events;
 
 import java.util.LinkedList;
@@ -20,7 +15,7 @@ public class DomainEvents {
     handlers.remove(handler);
   }
   
-  public static <T extends Event> void raise(T event) {
+  public static <E extends Event> void raise(E event) {
     for (Handler handler : handlers) {
       if (handler.handles(event.getClass())) {
         handler.handle(event);
--- src/main/java/xwizard/karteczki/gui/SWTHelloWorld.java
+++ src/main/java/xwizard/karteczki/gui/SWTHelloWorld.java
@@ -5,17 +5,21 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import java.util.ResourceBundle;
 
 /*
  * From http://www.mkyong.com/swt/swt-hello-world-example/
  */
 public class SWTHelloWorld {
+  private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("xwizard.karteczki.gui.messages"); //$NON-NLS-1$
  
 public static void main (String [] args) {
     Display display = new Display ();
     Shell shell = new Shell(display);
+    shell.setSize(152, 105);
  
     Text helloWorldTest = new Text(shell, SWT.NONE);
+    helloWorldTest.setLocation(40, 5);
     helloWorldTest.setText("Hello World SWT");
     helloWorldTest.pack();
     
