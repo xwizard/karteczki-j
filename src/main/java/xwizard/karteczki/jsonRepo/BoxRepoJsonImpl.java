deb5d9906e12803e36e11bf4e745a65d49f52d4b
xwizard <xwizard@server>
2015-12-03 22:44:17 +0100

    Time to write data backend!

---
+++ src/main/java/xwizard/karteczki/jsonRepo/BoxRepoJsonImpl.java
@@ -0,0 +1,28 @@
+package xwizard.karteczki.jsonRepo;
+
+import java.util.UUID;
+
+import xwizard.karteczki.quiz.Box;
+import xwizard.karteczki.repos.BoxRepo;
+
+public class BoxRepoJsonImpl implements BoxRepo {
+
+  @Override
+  public void save(Box entity) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Box get(UUID id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void delete(UUID id) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
--- src/main/java/xwizard/karteczki/repos/Repo.java
+++ src/main/java/xwizard/karteczki/repos/Repo.java
@@ -4,9 +4,9 @@ import java.util.UUID;
 
 interface Repo<T> {
 
-  abstract void save(T entity);
+  void save(T entity);
 
-  abstract T get(UUID id);
+  T get(UUID id);
 
-  abstract void delete(UUID id);
+  void delete(UUID id);
 }
