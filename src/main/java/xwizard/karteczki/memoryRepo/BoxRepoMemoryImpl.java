b8436f23c855dd62a75ce04ba0050b8e734c9cc8
xwizard <xwizard@server>
2015-12-06 13:12:04 +0100

    box repo test stub

--- src/main/java/xwizard/karteczki/memoryRepo/BoxRepoMemoryImpl.java
+++ src/main/java/xwizard/karteczki/memoryRepo/BoxRepoMemoryImpl.java
@@ -1,14 +1,20 @@
 package xwizard.karteczki.memoryRepo;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
 
 import xwizard.karteczki.quiz.Box;
+import xwizard.karteczki.repo.jsonInit.JsonRepoInitializer;
 import xwizard.karteczki.repos.BoxRepo;
 
 public class BoxRepoMemoryImpl implements BoxRepo {
   
   private Map<UUID, Box> boxes;
+
+  public BoxRepoMemoryImpl(JsonRepoInitializer<UUID, Box> init) {
+    boxes = Collections.unmodifiableMap(init.getValues());
+  }
 
   @Override
   public void save(Box entity) {
--- src/test/java/xwizard/karteczki/memoryRepo/BoxRepoMemoryImplTest.java
+++ src/test/java/xwizard/karteczki/memoryRepo/BoxRepoMemoryImplTest.java
@@ -1,14 +1,30 @@
 package xwizard.karteczki.memoryRepo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
 import org.junit.Before;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import xwizard.karteczki.quiz.Box;
+import xwizard.karteczki.repo.jsonInit.JsonRepoInitializer;
 import xwizard.karteczki.repos.BoxRepo;
 
 public class BoxRepoMemoryImplTest {
-  BoxRepo repo;
+  private static final UUID BOXID = UUID.fromString("8d467d29-59ad-4541-8dd8-83db5eda08a6");
+  private BoxRepo repo;
   
   @Before
-  public void before() {
-    repo = new BoxRepoMemoryImpl();
+  public void before() throws IOException {
+    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("box.json");
+    TypeReference<List<Box>> typeReference = new TypeReference<List<Box>>() {};
+    JsonRepoInitializer<UUID, Box> init = new JsonRepoInitializer<>(inputStream, typeReference);
+    inputStream.close();
+    repo = new BoxRepoMemoryImpl(init);
   }
+  
+  
 }
